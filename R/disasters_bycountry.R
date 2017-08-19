dd6a24309e02cb977b1bb3d4afd1546116f12f61
Thomas Maher <botanyhelp@gmail.com>
2017-08-19 15:19:47 -0400

    tried to fix misplaced @export

--- R/disasters_bycountry.R
+++ R/disasters_bycountry.R
@@ -1,10 +1,10 @@
 #' disasters_bycountry function
-#' @export
 #'
 #' This function allows you to find disasters by Country
 #'
 #' @param exactcountry Defaults to "Bangladesh"
 #' disasters_bycountry()
+#' @export
 
 disasters_bycountry <- function(exactcountry="Bangladesh") {
     Remdat[Remdat$Country==exactcountry,]
